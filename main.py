6029a42e5efca49faadf8a3f873f2a285b6c1b9a
bayre <benayre0@gmail.com>
2018-10-02 20:00:18 +0100

    Added q(1, s)

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ mySum = 4
 
 xs.append(-mySum)
 
-sumOfPositives = sum(filter(lambda x: x > 0, xs))
+a = sum(filter(lambda x: x < 0, xs))
+b = sum(filter(lambda x: x > 0, xs))
 
 q = {}
+
+for s in range(a, b + 1):
+    q[(1, s)] = xs[0] == s
+
+print(q)
+
+q = {}
+
+# print(sorted(xs))
