a0205c883acd86685a1d63f77db5d11e87cf88f0
Ben Ayre <b.ayre@globalcanopy.org>
2018-10-03 21:47:32 +0100

    Add subset list as argument of q

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ mySum = 4
 
 xs.append(-mySum)
 
-
-def q(i, s):
+def q(i, s, subset):
     if i == 0:
         r = xs[i] == s
         if r:
-            print(xs[i])
+            subset.append(xs[i])
         return r
     else:
-        if q(i - 1, s - xs[i]):
-            print(xs[i])
+        if q(i - 1, s - xs[i], subset):
+            subset.append(xs[i])
             return True
         else:
-            return q(i - 1, s) or xs[i] == s
+            return q(i - 1, s, subset) or xs[i] == s
 
+subset = []
 
-q(len(xs) - 1, 0)
+q(len(xs) - 1, 0, subset)
+
+print(subset)
