f621fba30141d9096fb244345b03d6f6aaa4cbd7
Nic Bricknell <_@nbrck.nl>
2018-10-06 22:41:18 +0100

    Make tree doubly linked

--- main.py
+++ main.py
@@ -6,32 +6,42 @@ xs.append(-mySum)
 
 
 class Node:
-    def __init__(self, i, s, left, right, truth):
+    def __init__(self, i, s, truth, parent):
         self.i = i
         self.s = s
-        self.left = left
-        self.right = right
+        self.parent = None
+        self.left = None
+        self.right = None
         self.truth = truth
 
-    def __repr__(self, depth = 1):
+    def set_children(self, left, right):
+        self.left = left
+        self.right = right
+
+    def __repr__(self, depth=1):
         return_string = [str([self.i, self.s, self.truth])]
         if self.truth:
             return_string.append(" <-------")
         for child in filter(lambda x: x is not None, [self.left, self.right]):
             if child.truth:
-                return_string.extend(["\n", "   " * (depth+1), child.__repr__(depth+1)])
+                return_string.extend(
+                    ["\n", "   " * (depth+1), child.__repr__(depth+1)])
             else:
-                return_string.extend(["\n", "   " * (depth+1), child.__repr__(depth+1)])
+                return_string.extend(
+                    ["\n", "   " * (depth+1), child.__repr__(depth+1)])
         return "".join(return_string)
 
-# Int -> Int -> Node
-def q(i, s):
+
+# Int -> Int -> Maybe Node -> Node
+def q(i, s, parent):
     if i == 0:
-        return Node(i, s, None, None, xs[i] == s)
+        return Node(i, s, xs[i] == s, parent)
     else:
-        left = q(i - 1, s)
-        right = q(i - 1, s - xs[i])
-        return Node(i, s, left, right, xs[i] == s)
+        n = Node(i, s, xs[i] == s, parent)
+        left = q(i - 1, s, n)
+        right = q(i - 1, s - xs[i], n)
+        n.set_children(left, right)
+        return n
 
 
-print(q(len(xs) - 1, 0))
+print(q(len(xs) - 1, 0, None))
